deaf0098b59c69b55e6582c0c98a2c408d0e9503
[Jin] <hahn23@my.bcit.ca>
2024-02-21 14:55:02 -0800

    removed app.ts app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const parser = new CsvMenuParser();
-const textWriter = new TextWriter();
-const htmlWriter = new HtmlWriter();
-
-// .txt 파일로 출력
-parser.writeMenu(textWriter);
-
-// .html 파일로 출력
-parser.writeMenu(htmlWriter);
